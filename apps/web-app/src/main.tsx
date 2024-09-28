de03029237270dbfc8ad6511e7a5136951612735
Nestor Mosquera <yunsde18@gmail.com>
2024-09-28 14:48:19 -0500

    chore: landing page

--- apps/web-app/src/main.tsx
+++ apps/web-app/src/main.tsx
@@ -4,11 +4,23 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { buildProvidersTree } from '@helebba/design-system/common';
 import { Modal } from '@helebba/design-system/web';
 import { AppRouter } from './router';
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import '@helebba/design-system/web/styles/index.css';
 
-const queryClient = new QueryClient();
+const { VITE_GOOGLE_CLIENT_ID } = import.meta.env;
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: true, 
+      retry: 1, 
+      staleTime: 300000,
+    },
+  },
+});
+
 const ProvidersTree = buildProvidersTree([
   [QueryClientProvider, { client: queryClient }],
+  [GoogleOAuthProvider, { clientId: VITE_GOOGLE_CLIENT_ID }],
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
