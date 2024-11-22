f963a86d89d58da653492fd7f9487e5133c083c5
Bitter <macbookpro13m12020@Bitter.local>
2024-11-21 19:58:26 -0500

    chore: remove has router

--- apps/web-app/src/router/AppRouter.tsx
+++ apps/web-app/src/router/AppRouter.tsx
@@ -1,18 +1,13 @@
 import {
   RouterProvider,
   createBrowserRouter,
-  createHashRouter,
 } from 'react-router-dom';
 import { routes } from './routes';
-import { hashRoutes } from './hash-routes';
 
 const router = createBrowserRouter(routes);
-const hashRouter = createHashRouter(hashRoutes);
-
 const AppRouter = () => {
   return (
     <>
-      <RouterProvider router={hashRouter} />
       <RouterProvider router={router} />
     </>
   );
