8bca97ff1b08b41b8aced31305b96b3087b2411c
Bitter <yunsde18@gmail.com>
2024-11-26 19:42:53 -0500

    chore: info

--- apps/web/src/sections/Home/BlackFriday/index.tsx
+++ apps/web/src/sections/Home/BlackFriday/index.tsx
@@ -7,7 +7,13 @@ const BlackFriday = () => {
     return (
         <div className={styles.container}>
             <div className={styles.content} >
-                <h2><span>Black Friday<Dot size={90} color='#F13A39' /></span></h2>
+                <h2>
+                    <div className={styles.offer}>
+                        <span>75%</span>
+                        <div className={styles.discount}>descuento</div>
+                    </div>
+                    <span>Black Friday<Dot size={90} color='#F13A39' /></span>
+                </h2>
                 <p>Hacer crecer tu negocio no cuesta tanto.</p>
                 <p>Tu plan de Helebba al 75% de descuento</p>
                 <p>durante esta semama.</p>
